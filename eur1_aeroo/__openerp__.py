aa58f731eec739f4469da34d69c88bced164a1ac
Alexis de Lattre <alexis.delattre@akretion.com>
2017-02-14 18:43:58 +0100

    Improve EUR.1 report

--- eur1_aeroo/__openerp__.py
+++ eur1_aeroo/__openerp__.py
@@ -1,45 +1,29 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    report_eur1 module for Odoo
-#    Copyright (C) 2012-2015 Akretion (www.akretion.com)
-#    @author: Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2012-2017 Akretion (www.akretion.com)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 
 {
-    'name': 'EUR.1 report',
-    'version': '1.0',
-    'category': 'Inventory, Logistic, Storage',
+    'name': 'EUR.1 aeroo report',
+    'version': '8.0.1.0.0',
+    'category': 'Warehouse Management',
     'license': 'AGPL-3',
-    'summary': 'Adds EUR.1 report on stock picking',
+    'summary': 'Adds EUR.1 report on pickings',
     'description': """
-This module adds an EUR.1 report on stock picking, that can be directly
-printed on the official EUR.1 form.
+EUR.1 Aeroo report
+==================
 
-This module has been written by Alexis de Lattre
-from Akretion <alexis.delattre@akretion.com>.
+This module adds an EUR.1 report (https://en.wikipedia.org/wiki/EUR.1_movement_certificate) in Aeroo format (ODT output) on pickings. This report is designed to be printed directly on the official EUR.1 paper form.
+
+This module has been written by Alexis de Lattre from Akretion
+<alexis.delattre@akretion.com>.
     """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com/',
     'depends': [
-        'intrastat_product',
+        'product_harmonized_system',
+        'intrastat_base',
         'report_aeroo',
-        'sale',  # we display some fields that are declared in "sale"
+        'stock',
         ],
     'data': ['report.xml'],
     'installable': True,
---
+++ eur1_aeroo/stock.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+# © 2012-2017 Akretion (www.akretion.com)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+
+from openerp import models, fields, api, _
+from openerp.exceptions import Warning as UserError
+import logging
+logger = logging.getLogger(__name__)
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.multi
+    def report_eur1_lines(self):
+        self.ensure_one()
+        if self.picking_type_code != 'outgoing':
+            raise UserError(_(
+                "Picking %s is not an outgoing picking, so printing an EUR.1 "
+                "doesn't make sense.") % self.name)
+        if not self.partner_id:
+            raise UserError(_(
+                "Missing partner on picking %s.") % self.name)
+        if not self.partner_id.country_id:
+            raise UserError(_(
+                "Country is missing on the delivery address of picking %s.")
+                % self.name)
+        if self.partner_id.country_id.intrastat:
+            raise UserError(_(
+                "The delivery address of the picking %s is located in %s "
+                "which is an EU country, so it doesn't make sense to print "
+                "an EUR.1.") % (self.name, self.partner_id.country_id.name))
+        res = {}
+        # key = (product, lot, order)
+        # value: weight
+        weight_uom_categ = self.env.ref('product.product_uom_categ_kgm')
+        unit_uom_categ = self.env.ref('product.product_uom_categ_unit')
+        kg_uom = self.env.ref('product.product_uom_kgm')
+        puo = self.env['product.uom']
+        if not self.pack_operation_ids:
+            raise UserError(_(
+                "There are no packing operations on picking %s. They are "
+                "generated when the picking is transfered.") % self.name)
+        i = 0
+        for pack in self.pack_operation_ids:
+            product = pack.product_id
+            if not product.origin_country_id:
+                raise UserError(_(
+                    "Missing country of origin on product '%s'")
+                    % product.name_get()[0][1])
+            if not product.origin_country_id.intrastat:
+                logger.info(
+                    'Skipping product %s because his country of origin is %s',
+                    product.name_get()[0][1],
+                    product.origin_country_id.name)
+                continue
+            if product.uom_id == kg_uom:
+                qty = pack.product_qty
+                uom = kg_uom
+            elif product.uom_id.category_id == weight_uom_categ:
+                qty = puo.compute(product.uom_id, pack.product_qty, kg_uom)
+                uom = kg_uom
+            elif product.uom_id.category_id == unit_uom_categ:
+                qty = pack.product_qty * product.weight
+                uom = kg_uom
+            else:
+                qty = pack.product_qty
+                uom = product.uom_id
+            if (product, pack.lot_id, uom) in res:
+                res[(product, pack.lot_id, uom)] += qty
+            else:
+                res[(product, pack.lot_id, uom)] = qty
+        if not res:
+            raise UserError(_(
+                "On picking %s, the are no products made in EU.") % self.name)
+        fres = []
+        for (product, lot, uom), qty in res.iteritems():
+            fres.append({
+                'product': product,
+                'lot': lot,
+                'uom': uom,
+                'qty': qty,
+                })
+        return fres
